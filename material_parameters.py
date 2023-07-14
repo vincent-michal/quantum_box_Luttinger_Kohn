7f75d67085882f3345ec3d21657482701951377b
vincent-michal <123947229+vincent-michal@users.noreply.github.com>
2023-07-14 13:41:01 +0100

    Add files via upload

--- material_parameters.py
+++ material_parameters.py
@@ -1,9 +1,9 @@
-#Luttinger parameters gamma1 gamma2 gamma3 kappa qu bv nu=2*c_12/c_11 / materials Si Ge InP GaAs InAs InSb
+#Luttinger parameters gamma_1 gamma_2 gamma_3 kappa qu bv nu=2*c_12/c_11 / materials Si Ge InP GaAs InAs InSb
 parameters = [
     {"Material": "Si", "ga1": 4.285, "ga2": 0.339, "ga3": 1.446, "ka": -0.42, "qu": 0.01, "bv": -2.1, "nu": 0.77},
     {"Material": "Ge", "ga1": 13.38, "ga2": 4.24, "ga3": 5.69, "ka": 3.41, "qu": 0.06, "bv": -2.86, "nu": 0.73},
     {"Material": "InP", "ga1": 4.95, "ga2": 1.65, "ga3": 2.35, "ka": 0.97, "qu": 0},
     {"Material": "GaAs", "ga1": 6.85, "ga2": 2.1, "ga3": 2.9, "ka": 1.2, "qu": 0.01},
-    {"Material": "InAs", "ga1": 20.4, "ga2": 8.3, "ga3": 9.1, "ka": 7.6, "qu": 0.39},
+    {"Material": "InAs", "ga1": 20.4, "ga2": 8.3, "ga3": 9.1, "ka": 7.6, "qu": 0.39, "Ck": -1.12},
     {"Material": "InSb", "ga1": 37.1, "ga2": 16.5, "ga3": 17.7, "ka": 15.6, "qu": 0.39},
 ]
